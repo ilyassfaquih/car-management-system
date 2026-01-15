ca2176cb6b01a2e66c08a170bbf090e901ce2c6c
ilyassfaquih <ilyassfaquih3@gmail.com>
2026-01-15 09:51:52 +0100

    commit

--- rental-service/src/main/java/com/example/rentalservice/config/AppConfig.java
+++ rental-service/src/main/java/com/example/rentalservice/config/AppConfig.java
@@ -1,12 +1,15 @@
 package com.example.rentalservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced; // <-- HADI LI NAQSAK
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
+
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
